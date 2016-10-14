3dd0b45fd91855c8426caee72aeb6e663ef640e5
cahlen <cahlen@iastate.edu>
2016-10-14 16:22:03 -0500

    Adjusting datetime to central time zone.

--- standards/util.py
+++ standards/util.py
@@ -1,4 +1,5 @@
-import datetime 
+from datetime import datetime
+from pytz import timezone
 
 def log(msg):
-    print(str(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')) + ": " + str(msg) + "\n", flush=True)
+    print(str(datetime.now(timezone('US/Central')).strftime('%Y-%m-%d %H:%M:%S')) + ": " + str(msg) + "\n", flush=True)
