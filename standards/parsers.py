ff818c1036e141ad4643ca094a5572317fdd7ecc
Cahlen <nokeo08@gmail.com>
2017-11-26 21:14:14 -0600

    Added two tests to test Parsers.py
    - one_to_one_parser has 10 scenarios tested
    - chapter_paragraph_parser has 25 scenarios tested

--- standards/parsers.py
+++ standards/parsers.py
@@ -5,20 +5,23 @@ def one_to_one_parser(num_groups):
         args = numGroup.split(",")
         for i in args:
             i = "".join(i.split())
-            if '-' not in i and i.isdigit():
-                result.append([int(i), int(i)])
-            elif '-' not in i and not i.isdigit():
-                malformed = True
-            elif '-' in i:
-                split = i.split("-")
-                if len(split) == 2 and split[0].isdigit() and split[1].isdigit() and int(split[0]) < int(split[1]):
-                    result.append([int(split[0]), int(split[1])])
-                elif not split[0].isdigit() or not split[1].isdigit():
-                    malformed = True
+            if '-' not in i:
+                if i.isdigit():
+                    result.append([int(i), int(i)])  # 1
                 else:
-                    malformed = True
+                    malformed = True  # a
             else:
-                malformed = True
+                split = i.split("-")
+                if len(split) == 2:
+                    if split[0].isdigit() and split[1].isdigit():
+                        if int(split[0]) < int(split[1]):
+                            result.append([int(split[0]), int(split[1])])  # 1-2
+                        else:
+                            malformed = True  # 2-1
+                    else:
+                        malformed = True  # a-2, 1-b,
+                else:
+                    malformed = True  # -, 1-, -2, 1-2-3
     return result, malformed
 
 
@@ -31,39 +34,51 @@ def chapter_paragraph_parser(num_groups):
             i = "".join(i.split())
             if '-' in i:
                 split = i.split('-')
-                if len(split) == 2 and ':' in split[0]:
-                    left_split = split[0].split(':')
-                    if len(left_split) == 2 and left_split[0].isdigit() and left_split[1].isdigit():
-                        if ':' in split[1]:
-                            right_split = split[1].split(':')
-                            if len(right_split) == 2 and right_split[0].isdigit() and right_split[1].isdigit():
-                                result.append([int(left_split[0]),
-                                               int(left_split[1]),
-                                               int(right_split[0]),
-                                               int(right_split[1])])  # 1:2-3:4
+                if len(split) == 2:
+                    if ':' in split[0]:
+                        left_split = split[0].split(':')
+                        if len(left_split) == 2:
+                            if left_split[0].isdigit() and left_split[1].isdigit():
+                                if ':' in split[1]:
+                                    right_split = split[1].split(':')
+                                    if len(right_split) == 2:
+                                        if right_split[0].isdigit() and right_split[1].isdigit():
+                                            result.append([int(left_split[0]),
+                                                           int(left_split[1]),
+                                                           int(right_split[0]),
+                                                           int(right_split[1])])  # 1:2-2:1
+                                        else:
+                                            malformed = True  # 1:2-a:1, 1:2-2:b
+                                    else:
+                                        malformed = True  # 1:2-2:, 1:2-2:1:, 1:2-2:1:2
+                                elif split[1].isdigit():
+                                    result.append([int(left_split[0]),
+                                                   int(left_split[1]),
+                                                   int(left_split[0]),
+                                                   int(split[1])])  # 1:2-3
+                                else:
+                                    malformed = True  # 1:2-a
                             else:
-                                malformed = True
-                        elif split[1].isdigit():
-                            result.append([int(left_split[0]),
-                                           int(left_split[1]),
-                                           int(left_split[0]),
-                                           int(split[1])])  # 5:6-7
+                                malformed = True  # a:2-2:1, 1:b-2:1
                         else:
-                            malformed = True
+                            malformed = True  # 1:-2:1, 1:2:-2:1, 1:2:3-2:1
                     else:
-                        malformed = True
+                        malformed = True  # 1-2:1
                 else:
-                    malformed = True
+                    malformed = True  # -, 1:1-, -2:1, 1:1-2:1-3:1
             else:
                 if ':' in i:
                     split = i.split(":")
-                    if len(split) == 2 and split[0].isdigit() and split[1].isdigit():
-                        result.append([int(split[0]),
-                                       int(split[1]),
-                                       int(split[0]),
-                                       int(split[1])])  # 8:9
+                    if len(split) == 2:
+                        if split[0].isdigit() and split[1].isdigit():
+                            result.append([int(split[0]),
+                                           int(split[1]),
+                                           int(split[0]),
+                                           int(split[1])])  # 1:2
+                        else:
+                            malformed = True  # a:1, 1:b
                     else:
-                        malformed = True
+                        malformed = True  # 1:2:3, 1:, :
                 else:
-                    malformed = True
+                    malformed = True  # 1
     return result, malformed
