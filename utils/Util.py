507a85f6523dd4991d70d232f004f97ed2e2d395
Cahlen Brancheau <cbrancheau@wrberkley.com>
2018-08-16 06:39:09 -0500

    Fixes filepath issue

--- utils/Util.py
+++ utils/Util.py
@@ -1,5 +1,7 @@
 from datetime import datetime
 from json import load, dump
+import os
+import sys
 
 from pytz import timezone
 
@@ -8,11 +10,15 @@ def log(msg):
     print(str(datetime.now(timezone('US/Central')).strftime('%Y-%m-%d %H:%M:%S')) + ": " + str(msg) + "\n", flush=True)
 
 
+def __get_path__(std):
+    return os.path.join(sys.path[0], "standards/json/" + std.upper() + ".json")
+
+
 def load_json(std):
-    with(open("standards/json/" + std.upper() + ".json", "r")) as infile:
+    with(open(__get_path__(std), "r")) as infile:
         return load(infile)
 
 
 def dump_json(std, data):
-    with(open("standards/json/" + std.upper() + ".json", "w")) as outfile:
+    with(open(__get_path__(std), "w")) as outfile:
         return dump(data, outfile)
